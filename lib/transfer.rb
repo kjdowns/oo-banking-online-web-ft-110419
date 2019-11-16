64bf355156327682f66a53406d7c579ab23927be
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 04:30:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 
 class Transfer
   
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize
+  def initialize(sender, receiver, amount)
     
   end
   
