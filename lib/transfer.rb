2fcccecfffbf13b21b8dc7209208b68ab2d105a5
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 07:55:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
+    if self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "closed"
