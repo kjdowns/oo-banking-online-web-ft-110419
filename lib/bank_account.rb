43355ae9217f5b2065b84b2831897e8919e72582
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 04:14:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
-class BankAccount
 
+class BankAccount
+  
+  attr_accessor :balance, :status
+  attr_reader :name 
+  
+  def initalize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+  end
+  
 end
