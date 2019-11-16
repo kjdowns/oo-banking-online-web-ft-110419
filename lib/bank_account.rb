0f89aa1312c655652790fcd3969399cd9b1c4962
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 04:18:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -11,6 +11,15 @@ class BankAccount
   end
   
   def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    @status == open && @balance > 0 ? true : false
   end
   
 end
