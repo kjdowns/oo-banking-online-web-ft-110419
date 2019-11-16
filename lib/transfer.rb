12125ec22d8b0d45fe922f714e80d80126b9452d
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 04:31:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,10 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiver, amount)
-    
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
   end
   
 end
